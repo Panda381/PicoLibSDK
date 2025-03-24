cd229d2e2cd2985187cae6a4de59815b23083dc8
Miroslav Nemecek <85685522+Panda381@users.noreply.github.com>
2025-03-24 09:02:36 +0100

    Version 2.07

--- PicoPad/DEMO/HELLO/src/main.cpp
+++ PicoPad/DEMO/HELLO/src/main.cpp
@@ -9,10 +9,6 @@
 
 int main()
 {
-	volatile u16* a;
-	a = (volatile u16*)0x30000000;
-	volatile u16 b = *a;
-
 	// draw text
 	DrawText2("Hello World!", (WIDTH-12*8*2)/2, (HEIGHT-8*2)/2, COL_WHITE);
 	DispUpdate();
